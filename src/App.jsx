e5a71c96419eca68b488dac07dd322b66c2fdb5d
Santoshm <santoshm@Trinesiss-MacBook-Air.local>
2023-12-27 19:05:39 +0530

    added components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.module.css";
+import { Header } from "./components/Header/Header";
+
+function App() {
+  return (
+    <div> 
+      <Header />
+    </div>
+  );
+}
+
+export default App;
