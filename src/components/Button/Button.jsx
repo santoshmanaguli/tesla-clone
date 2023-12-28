3c09a40c93699fc325999d3aeb5874ac5c919ed3
Santoshm <santoshm@Trinesiss-MacBook-Air.local>
2023-12-28 19:01:04 +0530

    added first image and button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,5 @@
-import styles from './Button.module.css';
+import styles from "./Button.module.css";
 
 export const Button = (props) => {
-  return(
-    <div>
-      <button type='submit'>
-        {props.btnName}
-      </button>
-    </div>
-  )
-}
+  return <button type="submit" style={props.styles}>{props.btnName}</button>;
+};
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,19 +1,45 @@
 import React from "react";
-import styles from './Section.module.css'
-import Button from '../Button/Button'
+import styles from "./Section.module.css";
+import { Button } from "../Button/Button";
 
 export const Section = (props) => {
-  return(
-    <section className={styles.container} id={props.sectionId}>
-      <h1 className={styles.carTitle}>
-        {props.carTitle}
-      </h1>
-      <p>
-        {props.subTitle}
-      </p>
-      <Button>
+  const leftButton = {
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    color: "black",
+    borderRadius: "5px",
+    width: '20%',
+    height: '40px',
+    fontSize: '16px',
+    border: 'none',
+    fontWeight: 500,
+  };
 
-      </Button>
+  const rightButton = {
+    backgroundColor: 'rgba(34, 34, 34, 0.64)',
+    color: "#fff",
+    borderRadius: "5px",
+    width: '20%',
+    height: '40px',
+    fontSize: '16px',
+    border: 'none',
+    fontWeight: 500,
+  };
+
+  return (
+    <section className={styles.container} id={props.sectionId}>
+      <h1 className={styles.carTitle}>{props.carTitle}</h1>
+      <p>{props.subTitle}</p>
+      <div className={styles.btnsStyle}>
+        <div className={styles.btnDiv}>
+          <Button styles={leftButton} btnName="Explore Inventory"></Button>
+          <Button styles={rightButton} btnName="Demo Drive"></Button>
+        </div>
+        <p className={styles.termsAndConditionStyle}>
+          *Excludes taxes and fees with price subject to change. Available in
+          select states.
+          <a className={styles.detailsList}>&nbsp;See Details</a>
+        </p>
+      </div>
     </section>
-  )
-}
+  );
+};
